558b90ff3e560b10d0c576ee16854b3801d148bb
gaozhen1996 <3168175817@qq.com>
2019-12-04 14:57:06 +0800

    封装测试方法

--- src/main/java/com/gz/javastudy/springapp/TestMain.java
+++ src/main/java/com/gz/javastudy/springapp/TestMain.java
@@ -25,12 +25,14 @@ import com.gz.javastudy.springapp.service.StudentService;
 public class TestMain {
 
     public static void main(String[] args) {
-        //======================================测试单例对象中，含有非单例的属性==========================================
-        testingletonSonjectPrototypeField();
-        //===================================模仿mybatis，将接口注册到spring容器中======================================
+        //======================================测试单例对象中，含有非单例的属性===========================================
+//        testingletonSonjectPrototypeField();
+        //===================================模仿mybatis，将接口注册到spring容器中=======================================
 //        testSelfMyBatis();
         //===================================执行自定义的BeanFactoryPostProcessor====================================
 //        testExecuteCustomBeanFactoryPostProcessor();
+        //=======================模仿AOP,测试在方法前执行自定义的代码,注意：测试这个方法需要加上@EnableAOP注解========================
+        testMyAop();
     }
 
     /**
@@ -48,6 +50,28 @@ public class TestMain {
     }
 
     /**
+     * 模仿AOP,测试在方法前执行自定义的代码
+     */
+    private static void testMyAop(){
+    	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestMain.class);
+        System.out.println("===================================模仿AOP,测试在方法前执行自定义的代码==========================================");
+        StudentDao studentDao = (StudentDao) context.getBean("studentDaoImpl1");
+        studentDao.getStudentById();
+        context.close();
+    }
+    
+    /**
+     * 此方法的测试，详细请见https://github.com/gaozhen1996/study-note/blob/master/JavaEE/spring/spring%E6%B5%81%E7%A8%8B.md#2%E6%B5%81%E7%A8%8B-1
+     */
+    private static void testExecuteCustomBeanFactoryPostProcessor() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.addBeanFactoryPostProcessor(new CustomBeanFactoryPostProcessor());
+        context.register(TestMain.class);
+        context.refresh();
+        context.close();
+    }
+    
+    /**
      * 模仿mybatis，将接口注册到spring容器中
      */
     private static void testSelfMyBatis(){
@@ -55,17 +79,6 @@ public class TestMain {
         System.out.println("===================================模仿mybatis，将接口注册到spring容器中=======================================");
         StudentDao studentDao = (StudentDao) context.getBean("studentDao");
         studentDao.getStudentById();
-        context.close();
-    }
-    
-    /**
-     * 此方法的测试，详细请见
-     */
-    private static void testExecuteCustomBeanFactoryPostProcessor() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.addBeanFactoryPostProcessor(new CustomBeanFactoryPostProcessor());
-        context.register(TestMain.class);
-        context.refresh();
         context.close();
     }
 
