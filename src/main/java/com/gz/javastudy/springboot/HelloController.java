69772b04018a9be97222ab5c3880923ace1db3f5
gaozhen1996 <3168175817@qq.com>
2020-10-30 20:14:14 +0800

    完成模拟springboot

--- src/main/java/com/gz/javastudy/springboot/HelloController.java
+++ src/main/java/com/gz/javastudy/springboot/HelloController.java
@@ -1,9 +1,12 @@
 package com.gz.javastudy.springboot;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
@@ -16,8 +19,10 @@ public class HelloController {
 	
 	@ResponseBody
 	@RequestMapping("/hello")
-	public String hello(@RequestParam String name) {
-		return "Hello,"+name;
+	public HashMap<String, String> hello(@RequestParam String name) {
+		HashMap<String, String> returnData = new HashMap<String, String>();
+		returnData.put("hello", name);
+		return returnData;
 	}
 	
 	
--- src/main/java/com/gz/javastudy/springboot/SpringApplication.java
+++ src/main/java/com/gz/javastudy/springboot/SpringApplication.java
@@ -2,7 +2,11 @@ package com.gz.javastudy.springboot;
 
 import java.io.File;
 
+
+import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class SpringApplication {
 	
@@ -12,7 +16,20 @@ public class SpringApplication {
 		Tomcat tomcat = new Tomcat();
 		try {
 			tomcat.setPort(8080);
-			tomcat.addWebapp("/", WEB_ROOT);
+			tomcat.addContext("/", WEB_ROOT);
+			
+	    	System.out.println("====================init===============================");
+	        // Load Spring web application configuration
+	        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+	        context.register(AppConfig.class);
+	        
+	        // Create and register the DispatcherServlet
+	        DispatcherServlet servlet = new DispatcherServlet(context);
+	        Wrapper mvc = tomcat.addServlet("/", "app", servlet);
+	        mvc.setLoadOnStartup(1);
+	        mvc.addMapping("/");
+			
+			
 			tomcat.start();	
 			tomcat.getServer().await();
 		} catch (Exception e) {
