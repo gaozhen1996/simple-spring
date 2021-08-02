cb587e77cec64bf3f5a2c1b3cd5a57ebadf2d9cc
固守青城 <3168175817@qq.com>
2021-08-02 19:57:23 +0800

    netty http 数组复制bug

---
+++ study-java-netty-server/src/main/java/com/gz/javastudy/netty/cases/case2/HttpResponse.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.gz.javastudy.netty.cases.case2;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
+
+
+public class HttpResponse {
+	
+	private HttpHeaders header;
+	
+	private FullHttpResponse response;
+	
+	private byte [] body;
+	
+	public HttpResponse(FullHttpResponse response) {
+
+		this.header = response.headers();
+		this.response = response;
+		if (response.content() != null) {
+			body = new byte[response.content().readableBytes()];
+			response.content().getBytes(0, body);
+		}
+	}
+
+//	public HttpResponse(FullHttpResponse response) {
+//		this.header = response.headers();
+//		this.response = response;
+//	}
+	
+	public HttpHeaders header()
+	{
+		return header;
+	}
+
+//	public byte [] body()
+//	{
+//		return body = response.content() != null ?
+//				response.content().array() : null;
+//	}
+
+//	public byte [] body() {
+//		body = new byte[response.content().readableBytes()];
+//		response.content().getBytes(0, body);
+//		return body;
+//	}
+
+	public byte [] body(){
+		return body;
+	}
+}
